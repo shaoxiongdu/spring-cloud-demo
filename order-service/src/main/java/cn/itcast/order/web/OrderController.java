e058f61d5c233b43c373697270683352c052a742
= <dushaoxiong@lixiang.com>
2022-01-28 21:39:10 +0800

    nacos配置中心

--- order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -2,11 +2,17 @@ package cn.itcast.order.web;
 
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.service.OrderService;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * @author dushaoxiong
@@ -18,9 +24,17 @@ public class OrderController {
    @Autowired
    private OrderService orderService;
 
+   @NacosValue("${pattern.dateformat}")
+   private String dateFormat;
+
     @GetMapping("{orderId}")
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
         // 根据id查询订单并返回
         return orderService.queryOrderById(orderId);
+    }
+
+    @GetMapping("/now")
+    public String now(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateFormat));
     }
 }
